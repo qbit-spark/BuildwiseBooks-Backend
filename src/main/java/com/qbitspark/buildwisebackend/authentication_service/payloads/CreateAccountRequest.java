793d29fba8560f44bca5fca02ce31e5e438a3b39
frontpage656 <joshuasimon656@gmail.com>
2025-07-25 17:07:15 +0300

    Refined database configuration and improved validation:
    - Updated `application.properties` to use the local database as the default and removed unused database configurations.
    - Replaced `@Max` annotations with `@Size` in `CreateAccountRequest` for better clarity and validation consistency.
    - Added missing `@NotBlank` annotation for `email` field in `CreateAccountRequest`.

--- src/main/java/com/qbitspark/buildwisebackend/authentication_service/payloads/CreateAccountRequest.java
+++ src/main/java/com/qbitspark/buildwisebackend/authentication_service/payloads/CreateAccountRequest.java
@@ -12,7 +12,6 @@ public class CreateAccountRequest {
             regexp = "^\\+[1-9]\\d{1,14}$",
             message = "Phone number must be in valid international format (e.g., +1234567890)"
     )
-
     private String phoneNumber;
 
     // Password validation for strong password
@@ -23,20 +22,22 @@ public class CreateAccountRequest {
     )
     private String password;
 
+    @NotBlank(message = "Email is mandatory")
     @Email(message = "Email should be valid")
     private String email;
 
     @NotBlank(message = "First name is required")
-    @Max(value = 30, message = "First name should be less than 30 characters")
+    @Size(max = 30, message = "First name should be less than 30 characters")
     private String firstName;
-    @Max(value = 30, message = "Last name should be less than 30 characters")
+
     @NotBlank(message = "Last name is required")
+    @Size(max = 30, message = "Last name should be less than 30 characters")
     private String lastName;
+
     @NotBlank(message = "Middle name is required")
-    @Max(value = 30, message = "Middle name should be less than 30 characters")
+    @Size(max = 30, message = "Middle name should be less than 30 characters")
     private String middleName;
 
     @NotNull(message = "Verification channel is mandatory")
     private VerificationChannels verificationChannel;
-
 }
